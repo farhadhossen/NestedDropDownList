4359278b924c0bd03e66db8ec061a4d39e22d631
farhadhossen <imfarhad01@gmail.com>
2022-11-03 11:35:47 +0600

    click btn and set text

--- app/src/main/java/com/farhad/nesteddropdownlist/MainActivity.java
+++ app/src/main/java/com/farhad/nesteddropdownlist/MainActivity.java
@@ -6,29 +6,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
-    String[] courses;
+    String[] divName;
+    String[] divDetails;
+    String details;
+    Button btn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.idBtn);
+        tv = findViewById(R.id.idTv);
+
+
         Spinner spin = findViewById(R.id.coursesspinner);
         spin.setOnItemSelectedListener(this);
 
-        courses = getResources().getStringArray(R.array.division_name);
+        divName = getResources().getStringArray(R.array.division_name);
+        divDetails = getResources().getStringArray(R.array.division_details);
 
         ArrayAdapter ad
                 = new ArrayAdapter(
                 this,
                 android.R.layout.simple_spinner_item,
-                courses);
+                divName);
 
 
         ad.setDropDownViewResource(
@@ -39,6 +49,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // Spinner which binds data to spinner
         spin.setAdapter(ad);
 
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tv.setText(details);
+            }
+        });
+
+
+
+
+
     }
 
 
@@ -48,6 +69,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 //                        courses[position],
 //                        Toast.LENGTH_LONG)
 //                .show();
+
+        details = divDetails[position];
+
+        tv.setText("");
+
+
     }
 
     @Override
