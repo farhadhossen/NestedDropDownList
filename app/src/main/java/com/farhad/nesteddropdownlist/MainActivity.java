13c3c6918904b4c1e2a4f6a025383c5f37e56b19
farhadhossen <imfarhad01@gmail.com>
2022-11-03 11:20:34 +0600

    one spinner

--- app/src/main/java/com/farhad/nesteddropdownlist/MainActivity.java
+++ app/src/main/java/com/farhad/nesteddropdownlist/MainActivity.java
@@ -3,12 +3,55 @@ package com.farhad.nesteddropdownlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    String[] courses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spin = findViewById(R.id.coursesspinner);
+        spin.setOnItemSelectedListener(this);
+
+        courses = getResources().getStringArray(R.array.division_name);
+
+        ArrayAdapter ad
+                = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                courses);
+
+
+        ad.setDropDownViewResource(
+                android.R.layout
+                        .simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter (ad) data on the
+        // Spinner which binds data to spinner
+        spin.setAdapter(ad);
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        Toast.makeText(getApplicationContext(),
+//                        courses[position],
+//                        Toast.LENGTH_LONG)
+//                .show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
